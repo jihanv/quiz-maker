2f087e8eb07a1b32bf34725c176b200ddf37044d
jihanv <61974348+jihanv@users.noreply.github.com>
2026-04-19 20:03:12 +0900

    add loading state for vocabulary dictionary generation

--- components/cloze-generator/clozeInput.tsx
+++ components/cloze-generator/clozeInput.tsx
@@ -28,6 +28,7 @@ export default function ParagraphInput() {
 
     const [generatedTestData, setGeneratedTestData] = useState<MultipleChoiceData | null>(null);
     const [generatedVocabularyEntries, setGeneratedVocabularyEntries] = useState<VocabularyDictionaryEntry[]>([]);
+    const [isGeneratingVocabularyDictionary, setIsGeneratingVocabularyDictionary] = useState(false);
 
     const onSubmit = async (data: TParagraphSchema) => {
         await new Promise((resolve) => setTimeout(resolve, 1000))
